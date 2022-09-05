11d88ef9665d44127c5c14f8c2d9e07e66c4aba2
Yura <yuraborisov19@gmail.com>
2022-09-05 13:13:41 +0300

    updated styles in server-error page

--- src/pages/serverErrorPage/styles.tsx
+++ src/pages/serverErrorPage/styles.tsx
@@ -1,22 +1,16 @@
 import styled from "styled-components";
 
-const ServerErrorPageWrapper = styled.div`
-  width: 100vw;
+import { FlexContainer } from "components/flex-container/styles";
+
+const ServerErrorPageWrapper = styled(FlexContainer)`
   height: 100vh;
-  display: flex;
-  align-items: center;
-  justify-content: center;
 `;
 const ServerErrorPageContent = styled.div`
   & > p {
     font-size: 30px;
-    font-weight: 400;
-    font-family: Roboto;
     margin-bottom: 5px;
   }
   & > a {
-    display: inline-block;
-    text-decoration: none;
     font-size: 20px;
   }
 `;
