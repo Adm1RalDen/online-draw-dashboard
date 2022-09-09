057d7461bc0f0f2de1bb00781cbac5a7bd6d00ce
YuraStork <80415664+YuraStork@users.noreply.github.com>
2022-09-09 11:46:46 +0300

    fixed app load
    * fixed app load
    
    * fixed chat image error
    
    * updated type

--- src/pages/home/chat/const.ts
+++ src/pages/home/chat/const.ts
@@ -1,3 +1,4 @@
+import { HOST } from 'api/const'
 import { CHAT_ERROR_SOCKET, CHAT_MESSAGE_SOCKET, GET_CHAT_SOCKET } from 'const/sockets'
 import { FunctionWithParams } from 'types'
 import { SocketApp } from 'types/socket'
@@ -13,6 +14,10 @@ type Props = {
   id: string
 }
 
+// !!! when will fix problem static folder delete ALTERNATIVE_IMAGE and exchange on DEFAULT_IMAGE
+export const ALTERNATIVE_IMAGE =
+  'https://thumbs.dreamstime.com/b/default-avatar-profile-icon-vector-social-media-user-portrait-176256935.jpg'
+export const DEFAULT_IMAGE = `${HOST}/users/defaultUserImage.png`
 export const setConnectionChat = (data: Props) => {
   const { setIsLoading, setMessages, id, setMessageLoading, setError, socket } = data
 
--- src/pages/home/chat/index.tsx
+++ src/pages/home/chat/index.tsx
@@ -1,4 +1,5 @@
 import { nanoid } from '@reduxjs/toolkit'
+import { HOST } from 'api/const'
 import { CHAT_MESSAGE_SOCKET } from 'const/sockets'
 import { useSocket } from 'hooks/useSocket'
 import { useEffect, useRef, useState } from 'react'
@@ -8,7 +9,7 @@ import { userInfoSelector } from 'store/selectors/user.selector'
 import { LittleLoader } from 'components/loaders/littleLoader'
 
 import { ChatMessage } from '../types'
-import { clearConnectionChat, setConnectionChat } from './const'
+import { ALTERNATIVE_IMAGE, clearConnectionChat, setConnectionChat } from './const'
 import {
   ChatWrapper,
   Message,
@@ -27,7 +28,6 @@ export const Chat = () => {
   const { data } = useAppSelector(userInfoSelector)
   const chatRef = useRef<HTMLDivElement>(null)
   const [messageLoading, setMessageLoading] = useState(false)
-  const DEFAULT_IMAGE = 'http://localhost:5000/users/defaultUserImage.png'
 
   useEffect(() => {
     setConnectionChat({
@@ -59,6 +59,12 @@ export const Chat = () => {
     }
   }
 
+  const onError = (e: React.SyntheticEvent<HTMLImageElement, Event>) => {
+    const target = e.target as HTMLImageElement
+    target.onerror = null
+    target.src = ALTERNATIVE_IMAGE
+  }
+
   return (
     <ChatWrapper>
       <MessagesBlock ref={chatRef}>
@@ -71,11 +77,8 @@ export const Chat = () => {
             <MessagesWrapper key={nanoid()}>
               <div>
                 <img
-                  src={`http://localhost:5000/users/${msg.userId}/${msg.userId}_avatar.png`}
-                  onError={(e) => {
-                    ;(e.target as HTMLImageElement).onerror = null
-                    ;(e.target as HTMLImageElement).src = DEFAULT_IMAGE
-                  }}
+                  src={`${HOST}/users/${msg.userId}/${msg.userId}_avatar.png`}
+                  onError={onError}
                   width={30}
                   height={30}
                   alt={msg.name}
