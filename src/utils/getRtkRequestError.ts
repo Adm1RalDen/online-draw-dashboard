ba089887930beb25519169e86fd2747add4a9428
Yura <yuraborisov19@gmail.com>
2022-12-15 16:23:10 +0200

    DD-23 added some types

--- src/utils/getRtkRequestError.ts
+++ src/utils/getRtkRequestError.ts
@@ -1,15 +1,22 @@
 import { SerializedError } from '@reduxjs/toolkit'
 import { FetchBaseQueryError } from '@reduxjs/toolkit/dist/query'
+import { is, object, string } from 'superstruct'
 
 import { ErrorMessages } from 'const/enums'
 
 import { ServerResponseError } from 'types'
 
-type Parameters = FetchBaseQueryError | SerializedError | undefined
+type Parameters = FetchBaseQueryError | SerializedError | ServerResponseError | undefined
+
+const serverError = object({
+  data: object({
+    message: string()
+  })
+})
 
 export const getRtkRequestError = (err: Parameters) => {
-  if (err) {
-    return (err as ServerResponseError)?.data?.message || ErrorMessages.OCCURED_ERROR
+  if (is(err, serverError)) {
+    return err.data.message || ErrorMessages.OCCURED_ERROR
   }
 
   return ErrorMessages.OCCURED_ERROR
